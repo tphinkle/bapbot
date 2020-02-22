b733685c829f0f7c1490bc7431b8031b22ddfb30
prestonh <prestonh@zillowgroup.com>
2020-02-22 09:27:17 -0800

    Add setup and postgres functionos

--- bapbot/database/functions.py
+++ bapbot/database/functions.py
@@ -6,16 +6,61 @@ from sqlalchemy_utils import database_exists, create_database
 import psycopg2
 
 ## Globals
-username = 'prestonh'
-dbname = 'bapbot'
+username = 'bap'
+username = ''
+dbname = 'bapdb'
+
+port     = '5432'
+
+
+password = 'password'
+host     = 'localhost'
+
+
+class SQLHandle(object):
+    """
+    """
+
+    def __init__(self, server_connection):
+        """
+        """
+        self.con = server_connetion
+        self.cursor = self.con.cursor()
+
+    def query(self,, command):
+        '''
+        '''
+        self.cursor.execute(command)
+        results = SQLHandle.cursor.fetchall()
+
+        if len(results) == 0:
+            results = None
+
+        return results
 
 def _create_database():
     """
     """
-    engine = create_engine('postgres://%s@localhost/%s'%(username,dbname))
-    #print(engine.url)
+    engine = _get_engine(username, password, host, port, db_name)
 
     ## create a database (if it doesn't exist)
     if not database_exists(engine.url):
         create_database(engine.url)
     pass
+
+def _get_engine(username, password, host, port, db_name):
+    """
+    """
+
+    return create_engine(
+        'postgres://{}:{}@{}:{}/{}' \
+            .format(username, password, host, port, db_name))
+
+def _connect_to_server(db_name, username, password, host):
+    """
+    """
+    # Create connection and cursor object to insert info into db
+     return SqlHandle(
+         psycopg2.connect(database = db_name,
+                          user = username,
+                          password = password, host = host))
