02a2b6db88d7a86eca5dd5a49a6048ce9d3ed154
prestonh <prestonh@zillowgroup.com>
2020-02-23 13:00:31 -0800

    Update

--- bapbot/handlers/http_handler.py
+++ bapbot/handlers/http_handler.py
@@ -20,7 +20,7 @@ class HTTPHandler(object):
         """
         # Bapper
         data = REST.bap.POST.assemble(bapper, bappee, bap_type, timestamp)
-        requests.post(SERVER_URL, data=data)
+        requests.post('http://127.0.0.1', data=data)
 
     @staticmethod
     def receive_bap_event(self):
