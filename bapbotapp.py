9c1432fe1847deb55258b6c6f2f623443247aee4
prestonh <prestonh@zillowgroup.com>
2020-02-22 16:25:23 -0800

    Update

--- bapbotapp.py
+++ bapbotapp.py
@@ -20,19 +20,21 @@ POST = 'POST'
 # Configure the app
 app = Flask(__name__, instance_relative_config=True)
 
+# Set up the handlers
+bap_handler = handlers.BapHandler()
+
+@app.route('/')
+def home():
+    return 'Testing'
+
+@app.route('/bap', methods=(GET, POST))
+def bap():
+    if request.method == POST:
+        bap_handler.handle_bap_post(request.args)
+    return 'ok'
+
 if __name__ == '__main__':
-    # Set up the handlers
-    bap_handler = handlers.BapHandler()
 
-    @app.route('/')
-    def home():
-        return 'Testing'
-
-    @app.route('/bap', methods=(GET, POST))
-    def bap():
-        if request.method == POST:
-            bap_handler.handle_bap_post(request.args)
-        return 'ok'
 
     #@app.route('/bap', methods=(GET, POST))
     #def bap():
