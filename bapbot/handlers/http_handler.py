ca56c07b4e5f10b9893d7056c708709cef5126cc
prestonh <prestonh@zillowgroup.com>
2020-02-23 13:40:04 -0800

    Update

--- bapbot/handlers/http_handler.py
+++ bapbot/handlers/http_handler.py
@@ -20,7 +20,7 @@ class HTTPHandler(object):
         """
         # Bapper
         data = REST.bap.POST.assemble(bapper, bappee, bap_type, timestamp)
-        response = requests.post('http://127.0.0.1/{}'.format(REST.bap.ENDPOINT), data=data)
+        response = requests.post('http://127.0.0.1/{}'.format(REST.bap.ENDPOINT), json=data)
         return 'poosted!'
 
     @staticmethod
