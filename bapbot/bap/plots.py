63b6f7ce832f63b4fc43c626741b43ace7ce6aa2
prestonh <prestonh@zillowgroup.com>
2020-02-27 19:21:52 -0800

    Update

--- bapbot/bap/plots.py
+++ bapbot/bap/plots.py
@@ -11,7 +11,7 @@ import bokeh.resources
 def create_bapper_stats_plot(bappers, bap_counts):
     """
     """
-    plot = figure(xrange=bappers, )
+    plot = figure(x_range=bappers, )
     plot.vbar(x=bappers, top=bap_counts)
     plot_html = bokeh.embed.file_html(plot, bokeh.resources.CDN, title='bapper stats')
     return plot_html
